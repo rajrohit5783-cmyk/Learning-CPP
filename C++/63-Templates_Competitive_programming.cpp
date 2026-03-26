a99f7d5fbd57b5ea1136d2661211e22815c91098
Rohit Raj <rajrohit5783@gmail.com>
2026-03-26 14:40:40 +0530

    Initial Commit

---
+++ C++/63-Templates_Competitive_programming.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class vector {
+    int *arr;
+    int size;
+    public:
+};
+
+ 
+template <class T>
+class vector {
+    T *arr;
+    int size;
+    public:
+        vector(T* arr)[
+            //code
+        ]
+        //and many other methods
+    
+};
+ 
+int main() {
+    vector<int> myVec1();
+    vector<float> myVec2();
+    return 0;
+}
